fe1ab9eb9253d0f9e9f53104003a15777988b0fb
Morais-dev <kauamorais1415@gmail.com>
2025-03-15 23:40:06 -0300

    Adicionando botão de vender

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,26 +13,71 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
-    
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
+    conectaDAO conect;
+    PreparedStatement stmt;
+    ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+    public ProdutosDAO() {
+        this.conect = new conectaDAO();
+        conn = this.conect.conectar();
+    }
+    
+    
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        try{
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.execute();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso");
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Falha ao cadastrar produto");
+        }
+
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        String sql = "SELECT * FROM produtos";
+        try{
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                ProdutosDTO p = new ProdutosDTO();
+                p.setId(rs.getInt("id"));
+                p.setNome(rs.getString("nome"));
+                p.setValor(rs.getInt("valor"));
+                p.setStatus(rs.getString("status"));
+                listagem.add(p);
+            }
+            
+        }catch(SQLException sqle){
+            System.out.println("erro na listagem");
+        }
+       
         return listagem;
+    }
+    
+    public void venderProduto(int id){
+        String sql = "UPDATE produtos SET status = ? WHERE id = ?";
+        try{
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, "Vendido");
+            stmt.setInt(2, id);
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Venda concluída");
+        }catch(SQLException sqle){
+            JOptionPane.showMessageDialog(null, "Erro ao concluir venda");
+        }
     }
     
     
--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -18,7 +18,6 @@ public class listagemVIEW extends javax.swing.JFrame {
      */
     public listagemVIEW() {
         initComponents();
-        listarProdutos();
         preencherTabela();
     }
     
@@ -154,12 +153,11 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
+        int id = Integer.parseInt(id_produto_venda.getText());
         
         ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
+        produtosdao.venderProduto(id);
+        preencherTabela();
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
@@ -219,25 +217,5 @@ public class listagemVIEW extends javax.swing.JFrame {
     private javax.swing.JTable listaProdutos;
     // End of variables declaration//GEN-END:variables
 
-    private void listarProdutos(){
-        try {
-            ProdutosDAO produtosdao = new ProdutosDAO();
-            
-            DefaultTableModel model = (DefaultTableModel) listaProdutos.getModel();
-            model.setNumRows(0);
-            
-            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutos();
-            
-            for(int i = 0; i < listagem.size(); i++){
-                model.addRow(new Object[]{
-                    listagem.get(i).getId(),
-                    listagem.get(i).getNome(),
-                    listagem.get(i).getValor(),
-                    listagem.get(i).getStatus()
-                });
-            }
-        } catch (Exception e) {
-        }
-    
-    }
+
 }
